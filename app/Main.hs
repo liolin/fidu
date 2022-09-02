857339e067e9886c73caf371d1a86c503c7e45a1
Olivier Lischer <olivier.lischer@liolin.ch>
2022-09-02 16:26:14 +0200

    Working example for an example file

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,13 @@
 module Main (main) where
 
+import qualified Data.ByteString as B
+import qualified Crypto.Hash.SHA256 as SHA256
+import Data.Digest.Pure.SHA
+import qualified Data.Ascii.Word8 as C
 import Lib
 
 main :: IO ()
-main = someFunc
+main = do
+  s <- example
+  putStrLn s
+
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,18 @@
 module Lib
-    ( someFunc
+    ( example
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import qualified Data.ByteString.Lazy as B
+import Data.Digest.Pure.SHA
+
+
+example :: IO String
+example = fmap blub openExampleFile
+
+blub :: B.ByteString -> String
+blub = showDigest . sha256
+
+
+openExampleFile :: IO B.ByteString
+openExampleFile =  B.readFile "/home/liolin/code/fidu/arch.jpg"
+
